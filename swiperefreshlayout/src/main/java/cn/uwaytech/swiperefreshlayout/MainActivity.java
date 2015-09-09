941cb07c0dbd90cef0ecfd727a25149faa856185
YorkYan <codersimple@gmail.com>
2015-09-09 10:18:41 +0800

    display and cancel refresh
    Signed-off-by: YorkYan <codersimple@gmail.com>

---
+++ swiperefreshlayout/src/main/java/cn/uwaytech/swiperefreshlayout/MainActivity.java
@@ -0,0 +1,53 @@
+package cn.uwaytech.swiperefreshlayout;
+
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        // set refresh onRefreshListener
+        swipeRefreshLayout.setOnRefreshListener(this);
+        // set indicator colors
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_green_light, android.R.color.holo_orange_light);
+        // refresh when begin
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+            }
+        });
+
+        stopRefresh();
+    }
+
+    @Override
+    public void onRefresh() {
+        stopRefresh();
+    }
+
+    private void stopRefresh() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, 2000);
+    }
+
+
+}
