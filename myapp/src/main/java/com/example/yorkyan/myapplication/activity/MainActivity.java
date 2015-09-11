1446c03e726fd6443ff925741dce334b87d26432
YorkYan <codersimple@gmail.com>
2015-09-11 16:10:11 +0800

    set fullScreen programmly
    Signed-off-by: YorkYan <codersimple@gmail.com>

--- myapp/src/main/java/com/example/yorkyan/myapplication/activity/MainActivity.java
+++ myapp/src/main/java/com/example/yorkyan/myapplication/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.yorkyan.myapplication.activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
@@ -42,5 +45,30 @@ public class MainActivity extends BaseActivity {
         View decorView = getWindow().getDecorView();
         // Calling setSystemUiVisibility() with a value of 0 clears all flags.
         decorView.setSystemUiVisibility(0);
+    }
+
+    /**
+     * should be called before setContentView() method
+     */
+    public void setFullScreen() {
+        // If the Android version is lower than Jellybean, use this call to hide
+        // the status bar.
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            // because the theme is compat so we should getSupportActionBar() but getActionBar()
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+        }
     }
 }
