d4c53cf5bfde673fbb3059ff2941fe40f6113f56
YorkYan <codersimple@gmail.com>
2015-09-15 13:46:00 +0800

    RecyclerView adapter
    Signed-off-by: YorkYan <codersimple@gmail.com>

--- materialdesign/src/main/java/cn/uwaytech/materialdesign/MainActivity.java
+++ materialdesign/src/main/java/cn/uwaytech/materialdesign/MainActivity.java
@@ -2,36 +2,63 @@ package cn.uwaytech.materialdesign;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
+    private int count = 0;
+    private List<Integer> integers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        final Handler handler = new Handler();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // LinearLayout manager
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        // vertical for default
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        // horizontal
+        // linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
 
-        return super.onOptionsItemSelected(item);
+        // GridLayout manager
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+
+        // set layout manager
+        recyclerView.setLayoutManager(linearLayoutManager);
+        // set item animator
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        // set adapter
+        final RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(integers);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        integers.add(0, count++);
+                        recyclerViewAdapter.notifyItemInserted(0);
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 1000);
+            }
+        });
     }
 }
---
+++ materialdesign/src/main/java/cn/uwaytech/materialdesign/RecyclerViewAdapter.java
@@ -0,0 +1,48 @@
+package cn.uwaytech.materialdesign;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
+    private List<Integer> integers;
+
+    public RecyclerViewAdapter(List<Integer> integers) {
+        this.integers = integers;
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+
+            textView = (TextView) itemView.findViewById(android.R.id.text1);
+        }
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        // create a new view
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+        // set the view's size, margin, padding and layout parameters
+        // ...
+
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder recyclerViewHolder, int position) {
+        recyclerViewHolder.textView.setText(integers.get(position) + "");
+    }
+
+    @Override
+    public int getItemCount() {
+        return integers.size();
+    }
+}
